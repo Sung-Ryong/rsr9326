3705f73c4bd110e2e97a65ffe72be42a1eec06ff
user <user@user-PC>
2014-10-30 17:05:40 +0900

    commit

--- src/com/example/send/MainActivity.java
+++ src/com/example/send/MainActivity.java
@@ -1,18 +1,78 @@
 package com.example.send;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 
 
 public class MainActivity extends ActionBarActivity {
+	
+	private HorizontalScrollView _scroll;
+	private ViewGroup _layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        _scroll = (HorizontalScrollView) findViewById(R.id.horizontalview1);
+        _layout = (ViewGroup) findViewById(R.id.linearlayout1);	
+        
+        int kLen = _layout.getChildCount();
+        for (int i = 0; i < kLen; i++)
+        {
+            View v = _layout.getChildAt(i);
+            if (v.getClass() != Button.class)
+                return;
+             
+            v.setOnClickListener(onBtnClickListener);
+        }
+      
+        
     }
+    OnClickListener onBtnClickListener = new OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int[] lo = new int[2];
+            v.getLocationOnScreen(lo);
+             
+            // 버튼의 x 좌표
+            int posX = lo[0];
+             
+            // 버튼의 너비의 절반
+            int halfWidth = (int) (v.getWidth() * 0.5f);
+             
+            // 화면 너비
+            int screenWith = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getWidth();
+             
+            // 화면 너비의 절반
+            int halfScreenWidth = (int) (screenWith * 0.5f);
+             
+            // 화면의 가로축 중간 위치와 버튼의 중간 위치와의 차이를 구한다.
+            final int scroll = posX + halfWidth - halfScreenWidth;
+            _scroll.post(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    _scroll.smoothScrollBy(scroll, 0);
+                }
+            });
+        }
+    };
+    
+  
+ 
 
 
     @Override
